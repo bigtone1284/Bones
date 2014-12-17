b21d21b8b054f36a93b7247c95b1aa5cc4bd7ca7
bigtone1284 <anthony.defreitas@gmail.com>
2014-12-17 17:29:44 -0500

    adding methods to game.

--- app/assets/javascripts/dominoGameClasses/game.js
+++ app/assets/javascripts/dominoGameClasses/game.js
@@ -5,6 +5,7 @@ function Game(playerArray) {
 	this.moves = 0;
 	this.active = true;
 	this.currentPlayer = null;
+	this.passes = 0;
 	this.dealBones(playerArray);
 }
 
@@ -21,22 +22,52 @@ Game.prototype.dealBones = function(playerNames) {
 	}.bind(this)); 
 };
 
-Game.prototype.switchPlayer = function() {
-	if (this.currentPlayer === this.hands.length - 1) {
-		return this.currentPlayer = 0;
-	} else {
-		return this.currentPlayer++;
-	}
-}
+Game.prototype.whoGoesFirst = function() {
+	var heaviestBones = new Hand;
+	this.hands.forEach(function(player, index) {
+		heaviestBones.bones.push(player["hand"].heaviestBone());
+	});
+	var heavy = heaviestBones.heaviestBone();
+	return this.currentPlayer = heaviestBones.bones.indexOf(heavy);
+};
 
-// Game.prototype.emptyHand = function() {		
-// 	for (var i = 0; i++; i < this.hands.length) {
-// 		if (this.hands[i][hand].isEmpty()) {
-// 			return this.hands[i][username];
+Game.prototype.switchPlayer = function() {
+	if (this.currentPlayer === null) {
+		return false;
+	} else {
+		if (this.currentPlayer === this.hands.length - 1) {
+			return this.currentPlayer = 0;
+		} else {
+			return this.currentPlayer += 1;
+		}
+	}
+};
+
+Game.prototype.emptyHand = function() {
+	for (var i = 0; i < this.hands.length; i++) {
+		if (this.hands[i]["hand"].isEmpty()) {
+			return i;
+		}
+	}
+	return false;
+};
+
+// Game.prototype.fewestPips = function() {
+// 	var pips = 999999;
+// 	var playerIndex = -1;
+// 	this.hands.forEach(function(player, index) {
+// 		if (player["hand"].totalPips() < pips) {
+// 			pips = player["hand"].totalPips();
+// 			playerIndex = index;
 // 		}
-// 	}
-// 	return false;
+// 		if (player["hand"].totalPips() === pips) {
+			
+
+// 		}
+// 	});
+// 	return playerIndex
 // };
+
 
 
 
