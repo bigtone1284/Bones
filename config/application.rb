de1dc89ae2bee608e30be1c044543a101c257c60
bigtone1284 <anthony.defreitas@gmail.com>
2015-01-03 23:43:04 -0500

    pushing fonts to heroku

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Bones
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf ) 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
